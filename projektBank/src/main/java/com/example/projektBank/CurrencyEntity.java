9aa235bed64cf2af173c783e403011d9ab352800
Danny <mcdanny@MacBook-Air-Danny.local>
2022-07-03 18:40:38 +0200

    pandathree

--- projektBank/src/main/java/com/example/projektBank/CurrencyEntity.java
+++ projektBank/src/main/java/com/example/projektBank/CurrencyEntity.java
@@ -14,14 +14,43 @@ public class CurrencyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
-    String wszystko;
+    String st_date;
+    String nd_date;
+    Date time;
 
+    float value;
 
-
-
-    public CurrencyEntity(String wszystko) {
-        this.wszystko = wszystko;
+    public CurrencyEntity( String st_date, String nd_date, float value) {
+        this.st_date = st_date;
+        this.nd_date = nd_date;
+        this.time = Date.from(Instant.now());
     }
 
+    public CurrencyEntity() {
 
+    }
+
+    public String getSt_date() {
+        return st_date;
+    }
+
+    public void setSt_date(String st_date) {
+        this.st_date = st_date;
+    }
+
+    public String getNd_date() {
+        return nd_date;
+    }
+
+    public void setNd_date(String nd_date) {
+        this.nd_date = nd_date;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
 }
